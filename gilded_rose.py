050c992bcf70fb36cb1cf2508ac19da946a1dcfe
Charlie <charlieavery123@hotmail.co.uk>
2024-02-10 07:22:17 +0000

    added update qulity method to Item

--- gilded_rose.py
+++ gilded_rose.py
@@ -12,6 +12,18 @@ class Item:
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
+    def update_quality(self):
+        if self.quality < 1:
+            self.sell_in -= 1
+            return
+
+        if self.sell_in > 0:
+            self.quality -= 1
+        else:
+            self.quality -= 2
+
+        self.sell_in -= 1
+
 
 class GildedRose(object):
     def __init__(self, items: list[Item]):
@@ -19,6 +31,7 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
+            # item.update_quality()
             if (
                 item.name != "Aged Brie"
                 and item.name != "Backstage passes to a TAFKAL80ETC concert"
