b0cb0f3d2990a1a43b23d983466f3cb438ffd4c2
Charlie <charlieavery123@hotmail.co.uk>
2024-02-10 07:22:20 +0000

    added tests

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -5,4 +5,26 @@ def test_foo():
     items = [Item("foo", 0, 0)]
     gilded_rose = GildedRose(items)
     gilded_rose.update_quality()
+    assert items[0].quality == 0
+    assert items[0].sell_in == -1
     assert "foo" == items[0].name
+
+
+def test_normal_quality_and_sellin_degradation():
+    items = [Item("test", 10, 5)]
+    shop = GildedRose(items)
+    shop.update_quality()
+
+    item = shop.items[0]
+    assert item.quality == 4
+    assert item.sell_in == 9
+
+
+def test_expired_quality_degredation():
+    items = [Item("test", 0, 5)]
+    shop = GildedRose(items)
+    shop.update_quality()
+
+    item = shop.items[0]
+    assert item.quality == 3
+    assert item.sell_in == -1
