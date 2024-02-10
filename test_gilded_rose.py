c310416e90463d09654c218fc553331cc4258178
Charlie <charlieavery123@hotmail.co.uk>
2024-02-10 12:39:00 +0000

    added tests for existing items

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -2,30 +2,97 @@ from src.gilded_rose import GildedRose
 from src.items import Item
 
 
-def test_foo():
-    items = [Item("foo", 0, 0)]
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    assert items[0].quality == 0
-    assert items[0].sell_in == -1
-    assert "foo" == items[0].name
-
-
-def test_normal_quality_and_sellin_degradation():
-    items = [Item("test", 10, 5)]
+def update_single_item(item: Item):
+    items = [item]
     shop = GildedRose(items)
     shop.update_quality()
 
-    item = shop.items[0]
+    return shop.items[0]
+
+
+def test_foo():
+    """
+    The Quality of an item is never negative
+    """
+    item = update_single_item(Item("foo", 0, 0))
+    assert item.quality == 0
+    assert item.sell_in == -1
+    assert "foo" == item.name
+
+
+def test_normal_quality_and_sellin_degradation():
+    """
+    At the end of each day our system lowers both values for every item
+
+    """
+    item = update_single_item(Item("test", 10, 5))
     assert item.quality == 4
     assert item.sell_in == 9
 
 
 def test_expired_quality_degredation():
-    items = [Item("test", 0, 5)]
-    shop = GildedRose(items)
-    shop.update_quality()
+    """
+    Once the sell by date has passed, Quality degrades twice as fast
+    """
 
-    item = shop.items[0]
+    item = update_single_item(Item("test", 0, 5))
     assert item.quality == 3
     assert item.sell_in == -1
+
+
+def test_aged_brie_quality_increases():
+    """
+    “Aged Brie” actually increases in Quality the older it gets
+    """
+    item = update_single_item(Item("Aged Brie", 5, 10))
+    assert item.quality == 11
+    assert item.sell_in == 4
+
+
+def test_quality_max_fifty():
+    """
+    The Quality of an item is never more than 50
+    """
+    item = update_single_item(Item("Aged Brie", 5, 50))
+    assert item.quality == 50
+
+
+def test_sulfuras_dont_degrade():
+    """
+    “Sulfuras”, being a legendary item, never has to be sold or decreases in Quality
+    """
+    item = update_single_item(Item("Sulfuras, Hand of Ragnaros", 5, 5))
+    assert item.quality == 5
+    assert item.sell_in == 5
+
+
+def test_sulfuras_can_have_higher_quality():
+    item = update_single_item(Item("Sulfuras, Hand of Ragnaros", 5, 80))
+    assert item.quality == 80
+
+
+def test_backstage_passes():
+    """
+    “Backstage passes”, like aged brie, increases in Quality as its SellIn value approaches;
+    Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+    Quality drops to 0 after the concert
+    """
+    item = update_single_item(Item("Backstage passes to a TAFKAL80ETC concert", 15, 5))
+    assert item.quality == 6
+
+    item = update_single_item(Item("Backstage passes to a TAFKAL80ETC concert", 10, 5))
+    assert item.quality == 7
+
+    item = update_single_item(Item("Backstage passes to a TAFKAL80ETC concert", 5, 5))
+    assert item.quality == 8
+
+    item = update_single_item(Item("Backstage passes to a TAFKAL80ETC concert", 0, 5))
+    assert item.quality == 0
+
+
+# def test_conjured_items():
+#     """
+#     “Conjured” items degrade in Quality twice as fast as normal items
+#     """
+#     item = update_single_item(Item("Conjured", 15, 5))
+#     assert item.quality == 3
